bbc2b2b97b93d9331c919e6423cd85ea604acb14
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-05-03 23:51:49 -0300

    Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import '../index.css'
 import './Navbar.module.css'
+import Logo from '../assets/Where.svg'
+import Statistic from '../assets/Statistic.svg'
+import Calendar from '../assets/Calendar.svg'
 
 
 function Navbar() {
 
   return (
-      <div>
-      </div>
+        <nav>
+          <ul>
+            <li><a href=""><img src={Statistic} alt="Statistic" /></a></li>
+            <li><img src={Logo} alt="Where Logo" width="92" height="25" id='logo'/></li>
+            <li><a href=""><img src={Calendar} alt="Calendar" /></a></li>
+          </ul>
+        </nav>
   )
 }
 
